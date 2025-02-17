725f7441f258ed98943241be1a2aee0019710410
Guilherme Salvi <guisalvi@outlook.com>
2025-02-17 20:12:35 -0300

    chore: remove sample files

--- samples/WebApi/Resources/SharedResource.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace WebApi.Resources;
-
-// Dummy class to be used as a type argument for the factory.Create method
-#pragma warning disable S2094 // An abstract class should have both abstract and concrete methods
-public abstract class SharedResource;
