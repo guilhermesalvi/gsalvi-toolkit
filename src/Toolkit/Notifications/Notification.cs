d22977c7177a29f43877bf3b6439d29157444a67
Guilherme Salvi <guisalvi@outlook.com>
2025-03-26 17:38:28 -0300

    feat: first commit

---
+++ src/Toolkit/Notifications/Notification.cs
@@ -0,0 +1,7 @@
+﻿namespace GSalvi.Toolkit.Notifications;
+
+public readonly record struct Notification(string Key, string Value)
+{
+    public Guid Id { get; } = Guid.CreateVersion7();
+    public DateTimeOffset Timestamp { get; } = DateTimeOffset.UtcNow;
+}
---
+++ src/Toolkit/Notifications/NotificationExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+
+namespace GSalvi.Toolkit.Notifications;
+
+public static class NotificationExtensions
+{
+    private static void AddNotifications(
+        this IServiceCollection services)
+    {
+        services.AddScoped<NotificationManager>();
+    }
+
+    public static void AddLocalizedNotifications<TResource>(
+        this IServiceCollection services,
+        Action<NotificationOptions> setupAction)
+        where TResource : class
+    {
+        var notificationOptions = new NotificationOptions();
+        setupAction.Invoke(notificationOptions);
+
+        services
+            .Configure<RequestLocalizationOptions>(x =>
+            {
+                x.DefaultRequestCulture = notificationOptions.DefaultRequestCulture!;
+                x.SupportedCultures = notificationOptions.SupportedCultures;
+                x.SupportedUICultures = notificationOptions.SupportedCultures;
+            })
+            .AddLocalization();
+
+        services.AddScoped<LocalizedNotificationManager>(sp =>
+        {
+            var factory = sp.GetRequiredService<IStringLocalizerFactory>();
+            var localizer = factory.Create(
+                typeof(TResource).FullName!,
+                typeof(TResource).Assembly.GetName().Name!);
+            return new LocalizedNotificationManager(localizer);
+        });
+    }
+
+    public static void UseLocalizedNotifications(this WebApplication app) =>
+        app.UseRequestLocalization();
+}
